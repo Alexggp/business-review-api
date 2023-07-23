71e5a188fa5a3c34312a97f5b1a08fb28ce4c4a9
Alejandro <alex.ggperez@gmail.com>
2023-07-23 20:44:36 +0200

    feat: two business types

--- src/bussineses/domain/business.ts
+++ src/bussineses/domain/business.ts
@@ -1,8 +1,24 @@
-export class Business {
+export interface Business {
+  readonly id: string;
+  readonly name: string;
+  readonly email: string;
+  readonly numberOfReviews: number;
+}
+export class BusinessOnline implements Business {
   constructor(
     readonly id: string,
     readonly name: string,
     readonly website: string,
+    readonly email: string,
+    readonly numberOfReviews: number
+  ) {}
+}
+export class BusinessPhysical implements Business {
+  constructor(
+    readonly id: string,
+    readonly name: string,
+    readonly address: string,
+    readonly phone: number,
     readonly email: string,
     readonly numberOfReviews: number
   ) {}
